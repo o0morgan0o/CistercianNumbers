8074396e810eeee1fdfe0a340e49ab3b47a6325c
o0morgan0o <morgan.thibert@protonmail.com>
2020-11-08 20:53:08 +0100

    add svg save

---
+++ README.md
@@ -0,0 +1,7 @@
+# Cistercians Number Generator / Convertor
+
+This project is related to the recent video of [Numberphile](https://www.youtube.com/watch?v=9p55Qgt7Ciw) about the cryptic and mysterious numerical system of some secret cistercians monks. I was quite interested by this concept, and by the high compression of writing because each symbol can represent a number between 0 and 9999. So I did a simple converter decimal/Cistercian in order to generate SVG of the cisterians symbols for any reason.
+
+Here is the related video:
+
+[![IMAGE ALT TEXT HERE](https://img.youtube.com/vi/9p55Qgt7Ciw/0.jpg)](https://www.youtube.com/watch?v=9p55Qgt7Ciw)
--- main.js
+++ main.js
@@ -17,6 +17,53 @@ let inputNumberElement
 let symbolsPerLine = 4
 let cnv
 
+function saveSVGtoFile() {
+    console.log('saving svg...')
+    let svgTag = document.getElementById('defaultCanvas0')
+    let svgdata = svgTag.innerHTML
+    let blob = [svgdata]
+    let oMyBlob = new Blob(blob, { type: 'image/svg+xml' })
+    downloadBlob(oMyBlob, 'test.svg')
+}
+
+function downloadBlob(blob, filename) {
+    // Create an object URL for the blob object
+    const url = URL.createObjectURL(blob);
+
+    // Create a new anchor element
+    const a = document.createElement('a');
+
+    // Set the href and download attributes for the anchor element
+    // You can optionally set other attributes like `title`, etc
+    // Especially, if the anchor element will be attached to the DOM
+    a.href = url;
+    a.download = filename || 'download';
+
+    // Click handler that releases the object URL after the element has been clicked
+    // This is required for one-off downloads of the blob content
+    const clickHandler = () => {
+        setTimeout(() => {
+            URL.revokeObjectURL(url);
+            this.removeEventListener('click', clickHandler);
+        }, 150);
+    };
+
+    // Add the click event listener on the anchor element
+    // Comment out this line if you don't want a one-off download of the blob content
+    a.addEventListener('click', clickHandler, false);
+
+    // Programmatically trigger a click on the anchor element
+    // Useful if you want the download to happen automatically
+    // Without attaching the anchor element to the DOM
+    // Comment out this line if you don't want an automatic download of the blob content
+    a.click();
+
+    // Return the anchor element
+    // Useful if you want a reference to the element
+    // in order to attach it to the DOM or use it in some other way
+    return a;
+}
+
 function preload() {
     inputNumberElement = document.getElementById('number')
     inputNumber = inputNumberElement.value
@@ -29,15 +76,16 @@ function preload() {
 
 function updateCanvas() {
 
-    console.log('input number', inputNumber)
     let scisserionNum = new ScisserionEnsemble(inputNumber, [0, 0])
     let numberOfSymbols = scisserionNum.getNumberOfSymbols()
-    console.log(numberOfSymbols, ' symbols')
     let newCanvasDimensions = determineCanvasDimensions(scisserionNum.numberOfSymbols)
     let cWidth = newCanvasDimensions[0]
     let cHeight = newCanvasDimensions[1]
     // resizeCanvas(symbolWidth * numberOfSymbols + 20, symbolHeight + 40)
+    let myCanvas = document.getElementById('defaultCanvas0')
+    document.getElementById('canvasParent').appendChild(myCanvas)
     resizeCanvas(cWidth, cHeight)
+
     stroke(255, 255, 255)
     background('#333')
     scisserionNum.draw()
@@ -59,18 +107,14 @@ function determineCanvasDimensions(numberOfSymbols) {
     }
     return [newCanvasWidth, newCanvasHeight]
 
-
-
 }
 
 
 function setup() {
     cnv = createCanvas(400, 400, SVG); // aspect ratio for standard printing on the CNC
-    // cnv.parent('canvasParent')
     noLoop();
     strokeWeight(5)
     updateCanvas()
-    // blendMode(MULTIPLY)
     stroke(0);
     noFill();
 
@@ -87,7 +131,6 @@ class ScisserionEnsemble {
         this.symbolNum = this.array.join('')
         this.numberOfSymbols = 0
         this.index = 0
-        console.log('init', this.array, this.symbolNum)
 
 
         while (this.array.length > 0) {
@@ -117,7 +160,6 @@ class ScisserionEnsemble {
 class ScisserionSymbol {
 
     constructor(num, position, symbolWidth, symbolHeight, index) {
-        console.log(index)
         this.verticalExtension = 3
         this.index = index
         this.position = position
@@ -140,7 +182,6 @@ class ScisserionSymbol {
     calculatePositionOfSymbol() {
         let lineIndex = Math.trunc(this.index / symbolsPerLine)
         let columnIndex = this.index % symbolsPerLine
-        console.log('position symbol', lineIndex, columnIndex)
         this.position[0] = this.position[0] + (symbolWidth * columnIndex + horizontalSpace)
         this.position[1] = this.position[1] + ((symbolHeight * 1.5) * lineIndex + verticalSpace)
     }
