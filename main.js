115bba9b0ce7de96bdf6d7a11828057bb3640d51
o0morgan0o <morgan.thibert@protonmail.com>
2020-11-08 19:53:09 +0100

    add style and better functions

--- main.js
+++ main.js
@@ -9,12 +9,17 @@ let verticalSpace = 20
 let symbolWidth = 80
 let symbolHeight = 60
 let horizontalSpace = 20
+let maximumCanvasSize = 300
 
-let inputNumber = '1'
+let inputNumber = ''
 let inputNumberElement
+
+let symbolsPerLine = 4
+let cnv
 
 function preload() {
     inputNumberElement = document.getElementById('number')
+    inputNumber = inputNumberElement.value
     inputNumberElement.addEventListener('input', (event) => {
         console.log(event.target.value)
         inputNumber = event.target.value.toString()
@@ -23,21 +28,45 @@ function preload() {
 }
 
 function updateCanvas() {
-    // clear()
 
     console.log('input number', inputNumber)
-    let scisserionNum = new ScisserionEnsemble(inputNumber, [10, 10])
+    let scisserionNum = new ScisserionEnsemble(inputNumber, [0, 0])
     let numberOfSymbols = scisserionNum.getNumberOfSymbols()
     console.log(numberOfSymbols, ' symbols')
-    resizeCanvas(symbolWidth * numberOfSymbols + 20, symbolHeight + 40)
-    background(200)
+    let newCanvasDimensions = determineCanvasDimensions(scisserionNum.numberOfSymbols)
+    let cWidth = newCanvasDimensions[0]
+    let cHeight = newCanvasDimensions[1]
+    // resizeCanvas(symbolWidth * numberOfSymbols + 20, symbolHeight + 40)
+    resizeCanvas(cWidth, cHeight)
+    stroke(255, 255, 255)
+    background('#333')
     scisserionNum.draw()
+
+}
+
+function determineCanvasDimensions(numberOfSymbols) {
+    let newCanvasWidth
+    let newCanvasHeight
+    if (numberOfSymbols <= symbolsPerLine) {
+        newCanvasWidth = symbolWidth * numberOfSymbols + 2 * horizontalSpace
+        newCanvasHeight = symbolHeight + verticalSpace * 3
+    } else if (numberOfSymbols % symbolsPerLine === 0) {
+        newCanvasWidth = symbolWidth * symbolsPerLine + horizontalSpace * 2
+        newCanvasHeight = (Math.trunc(numberOfSymbols / symbolsPerLine)) * symbolHeight * 1.5 + verticalSpace * 2
+    } else {
+        newCanvasWidth = symbolWidth * symbolsPerLine + horizontalSpace * 2
+        newCanvasHeight = (Math.trunc(numberOfSymbols / symbolsPerLine) + 1) * symbolHeight * 1.5 + verticalSpace * 2
+    }
+    return [newCanvasWidth, newCanvasHeight]
+
+
 
 }
 
 
 function setup() {
-    createCanvas(400, 400); // aspect ratio for standard printing on the CNC
+    cnv = createCanvas(400, 400, SVG); // aspect ratio for standard printing on the CNC
+    // cnv.parent('canvasParent')
     noLoop();
     strokeWeight(5)
     updateCanvas()
@@ -57,6 +86,7 @@ class ScisserionEnsemble {
         this.symbolPosition = [...this.position]
         this.symbolNum = this.array.join('')
         this.numberOfSymbols = 0
+        this.index = 0
         console.log('init', this.array, this.symbolNum)
 
 
@@ -66,9 +96,8 @@ class ScisserionEnsemble {
 
             let scisserionNumber = this.array.splice(0, numToSplice)
             // console.log('before', scisserionNumber)
-            this.scisserionNums.push(new ScisserionSymbol(scisserionNumber, [...this.symbolPosition], this.symbolWidth, this.symbolHeight))
-            this.symbolPosition[0] += this.symbolWidth
-            // this.scisserionNums.push(new ScisserionSymbol(scisserionNumber, [random(width), random(height)]))
+            this.scisserionNums.push(new ScisserionSymbol(scisserionNumber, [...this.symbolPosition], this.symbolWidth, this.symbolHeight, this.index))
+            this.index++
 
         }
     }
@@ -87,17 +116,17 @@ class ScisserionEnsemble {
 
 class ScisserionSymbol {
 
-    constructor(num, position, symbolWidth, symbolHeight) {
+    constructor(num, position, symbolWidth, symbolHeight, index) {
+        console.log(index)
         this.verticalExtension = 3
+        this.index = index
         this.position = position
         // go to next line if too wide
         this.height = symbolWidth
         this.width = symbolHeight
-        // while (this.position[0] > width - this.symbolWidth) {
-        while (this.position[0] >= 800) {
-            this.position[0] -= 800
-            this.position[1] += this.height + 20
-            // console.log('reset at ', this.position)
+        while (this.position[0] >= maximumCanvasSize) {
+            this.position[0] -= maximumCanvasSize
+            // this.position[1] += this.height + verticalSpace
         }
         this.quadran = 'ne'
         this.array = num
@@ -105,8 +134,15 @@ class ScisserionSymbol {
         while (this.array.length < 4) {
             this.array.unshift('0') // fill with zero in order to keep 42 as 0042
         }
-        this.index = 0
+        this.calculatePositionOfSymbol()
         // console.log('new number ', this.array)
+    }
+    calculatePositionOfSymbol() {
+        let lineIndex = Math.trunc(this.index / symbolsPerLine)
+        let columnIndex = this.index % symbolsPerLine
+        console.log('position symbol', lineIndex, columnIndex)
+        this.position[0] = this.position[0] + (symbolWidth * columnIndex + horizontalSpace)
+        this.position[1] = this.position[1] + ((symbolHeight * 1.5) * lineIndex + verticalSpace)
     }
 
     draw() {
@@ -126,8 +162,6 @@ class ScisserionSymbol {
             }
             this.drawQuadraticDigit()
         }
-
-
     }
     drawQuadraticDigit() {
 
